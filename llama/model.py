a800c74430efc39f33156261d04720c7a27ed011
Ryu1845 <ryu@tpgjbo.xyz>
2023-08-29 15:55:49 +0200

    add rmsnorm

--- llama/model.py
+++ llama/model.py
@@ -5,8 +5,18 @@ from keras_core import ops, Layer
 
 
 class RMSNorm(Layer):
-    def __init__(self):
+    def __init__(self, eps: float = 1e-6):
         super().__init__()
+        self.eps = eps
+
+    def build(self, input_shape):
+        self.weight = self.add_weight(shape=input_shape, initializer="ones", trainable=True, name="weight")
+
+    def call(self, inputs):
+        def norm(x):
+            return x * ops.rsqrt(ops.mean(ops.square(x), axis=-1, keepdims=True) + self.eps)
+        output = ops.cast(norm(ops.cast(inputs, dtype="float32")), dtype=inputs.dtype)
+        return output * self.weight
 
 
 def get_model():
