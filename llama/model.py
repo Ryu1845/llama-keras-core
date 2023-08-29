b4c849ba249628b0e6e219be845c859a50bbe1c9
Sofian Mejjoute <77058942+Ryu1845@users.noreply.github.com>
2023-08-29 15:08:41 +0200

    Create model.py

---
+++ llama/model.py
@@ -0,0 +1,18 @@
+"""keras_core implementation of [Llama](https://arxiv.org/abs/2302.13971)
+Based on [minimal-llama](https://github.com/zphang/minimal-llama/blob/main/minimal_llama/model.py)"""
+import keras_core as keras
+from keras_core import ops, Layer
+
+
+class RMSNorm(Layer):
+    def __init__(self):
+        super().__init__()
+
+
+def get_model():
+    pass
+
+
+if __name__ == '__main__':
+    model = get_model()
+  
